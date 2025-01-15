aa29052603de4c86889a6abee226ee8bad87d2c9
Helen Tran <helentran64@gmail.com>
2025-01-15 16:23:54 -0500

    added svg icons to timeline

--- src/components/WorkExperiences.vue
+++ src/components/WorkExperiences.vue
@@ -8,6 +8,17 @@
         <span
           class="tw-absolute tw-flex tw-items-center tw-justify-center tw-w-6 tw-h-6 tw-bg-blue-100 tw-rounded-full tw--start-3 tw-ring-8 tw-ring-white tw-dark:ring-gray-900 tw-dark:bg-blue-900"
         >
+          <svg
+            class="tw-w-3 tw-h-3 tw-text-blue-800 tw-dark:text-blue-300"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
+            />
+          </svg>
         </span>
         <h3
           class="tw-flex tw-items-center tw-mb-1 tw-text-lg tw-font-semibold tw-text-gray-900 tw-dark:text-white"
@@ -32,7 +43,17 @@
         <span
           class="tw-absolute tw-flex tw-items-center tw-justify-center tw-w-6 tw-h-6 tw-bg-blue-100 tw-rounded-full tw--start-3 tw-ring-8 tw-ring-white tw-dark:ring-gray-900 tw-dark:bg-blue-900"
         >
-          <!-- Add icon picture here -->
+          <svg
+            class="tw-w-3 tw-h-3 tw-text-blue-800 tw-dark:text-blue-300"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
+            />
+          </svg>
         </span>
         <h3
           class="tw-mb-1 tw-text-lg tw-font-semibold tw-text-gray-900 tw-dark:text-white"
@@ -53,7 +74,17 @@
         <span
           class="tw-absolute tw-flex tw-items-center tw-justify-center tw-w-6 tw-h-6 tw-bg-blue-100 tw-rounded-full tw--start-3 tw-ring-8 tw-ring-white tw-dark:ring-gray-900 tw-dark:bg-blue-900"
         >
-          <!-- Add icon picture here -->
+          <svg
+            class="tw-w-3 tw-h-3 tw-text-blue-800 tw-dark:text-blue-300"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
+            />
+          </svg>
         </span>
         <h3
           class="tw-mb-1 tw-text-lg tw-font-semibold tw-text-gray-900 tw-dark:text-white"
@@ -74,7 +105,17 @@
         <span
           class="tw-absolute tw-flex tw-items-center tw-justify-center tw-w-6 tw-h-6 tw-bg-blue-100 tw-rounded-full tw--start-3 tw-ring-8 tw-ring-white tw-dark:ring-gray-900 tw-dark:bg-blue-900"
         >
-          <!-- Add icon picture here -->
+          <svg
+            class="tw-w-3 tw-h-3 tw-text-blue-800 tw-dark:text-blue-300"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
+            />
+          </svg>
         </span>
         <h3
           class="tw-mb-1 tw-text-lg tw-font-semibold tw-text-gray-900 tw-dark:text-white"
