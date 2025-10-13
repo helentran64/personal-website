921c764e149b584815f0e89f2c5667429ca1fb8d
Helen Tran <helentran64@gmail.com>
2025-10-13 18:11:45 -0400

    added base for deployment

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Homepage from "@/views/Homepage.vue";
 
 const router = createRouter({
-  history: createWebHistory("/personal-website/"),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
